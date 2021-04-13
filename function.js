ffffbcebc55b5deae21d29c1d46130fb03f84bf6
Chaeyeon KIM <kcy85078507@gmail.com>
2021-04-13 17:24:36 +0900

    2021-04-13

--- function.js
+++ function.js
@@ -1,13 +1,13 @@
 function fibo(num) { 
-  fiboArr = [];
+  fiboArr = []; //배열
 
-  if (num === 1) {
+  if (num === 1) { //조건문
     fiboArr = [0];
   } else if (num === 2) {
     fiboArr = [0, 1];
   } else {
     fiboArr = [0, 1];
-    for (let i = 2; i < num; i++) {
+    for (let i = 2; i < num; i++) { //반복문
       fiboArr.push(fiboArr[fiboArr.length - 2] + fiboArr[fiboArr.length - 1]);
     }
   }
